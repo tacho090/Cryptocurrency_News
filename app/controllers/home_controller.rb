945ca96cb13e0db8c6229ee6a66f26433a1ffdf7
pablomora <pmora1990@gmail.com>
2019-07-25 09:56:02 -0600

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+
+    #Grab news data
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @news = JSON.parse(@response) 
+
+    #Grab prices data
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS&tsyms=USD,EUR'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+
+  end
+
+  def prices
+    require 'net/http'
+    require 'json'
+
+    if @symbol
+      @symbol = params[:sym]
+      @symbol = @symbol.upcase
+
+      #Grab prices data
+      @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=USD,EUR'
+      @quote_uri = URI(@quote_url)
+      @quote_response = Net::HTTP.get(@quote_uri)
+      @quote = JSON.parse(@quote_response)
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/prices'
+  post '/home/prices' => 'home/prices'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
